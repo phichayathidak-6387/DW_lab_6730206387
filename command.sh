b8cae9d26be59b3437a722577c4db41717dd660d
Kingpai-6387 <phichayathida.k@kkumail.com>
2026-07-10 04:56:32 +0000

    add dbt

---
+++ command.sh
@@ -0,0 +1,9 @@
+# install dbt-duckdb
+pip install dbt-duckdb
+
+# initialize dbt project
+dbt init <dbt-project-nam>
+
+# jump to the dbt project directory and run dbt debug to check the configuration
+cd <dbt-project-name>
+dbt debug
